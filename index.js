726d4c9f683e0811086ebf6bba4c9503fc2ad312
Tuan Nguyen <nmtuan.dev@gmail.com>
2017-03-29 22:09:13 +0700

    Bump

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+var morgan = require('morgan');
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.status(200).end("OK"));
 app.get('/oauth', (req, res) => {
     console.log(req.query);
+    console.log(req.body);
     res.end();
 });
 
