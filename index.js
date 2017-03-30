2d95e5f804d18e439400df653dd41949b5d48ceb
Tuan Nguyen <nmtuan.dev@gmail.com>
2017-03-30 09:30:02 +0700

    Bump

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var morgan = require('morgan');
 var express = require('express');
+var request = require('request');
 var bodyParser = require('body-parser');
 
 var app = express();
@@ -15,9 +16,15 @@ app.get('/oauth', (req, res) => {
     res.end();
 });
 app.post('/subscribe', (req, res) => {
-    console.log(req.query);
-    console.log(req.body);
-    res.status(200).end();
+    var subscribeURL = req.body.SubscribeURL; 
+    request.get(subscribeURL, (err, response, body) => {
+        if (err)
+            return console.log(err);
+        
+        console.log(body);
+        console.log('Subscription success');
+        res.status(200).end();
+    });
 });
 
 var PORT = process.env.PORT || 3000;
