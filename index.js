3ccb08d43cb8b53b0dddd84a722142f2d1cff52b
Tuan Nguyen <nmtuan.dev@gmail.com>
2017-03-30 17:11:12 +0700

    .

--- index.js
+++ index.js
@@ -75,8 +75,8 @@ router.post('/subscription/endpoint', (req, res, next) => {
 //===========================================================
 var app = express();
 app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 app.use((err, req, res, next) => {
     console.log(err);
