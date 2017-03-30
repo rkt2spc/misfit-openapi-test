d0352dc0e059f9ea9426c47e5fd1c959fc6af8f5
Tuan Nguyen <nmtuan.dev@gmail.com>
2017-03-30 15:00:52 +0700

    .

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 //===========================================================
 var router = express.Router();
 router.get('/', (req, res) => res.status(200).end("OK"));
-router.get('/login', (req, res) => res.redirect('https://api.misfitwearables.com/auth/dialog/authorize?response_type=token&client_id=uDHmdBZVZakB8jL2&client_secret=UlKUAiI2SW9RlK1d3wTlT5ZF9mM8appW&redirect_uri=mfc-P8ABAdMAhoLuBBoO://authorize&scope=public,birthday,email'));
+router.get('/login', (req, res) => res.redirect('https://api.misfitwearables.com/auth/dialog/authorize?response_type=token&client_id=uDHmdBZVZakB8jL2&client_secret=UlKUAiI2SW9RlK1d3wTlT5ZF9mM8appW&redirect_uri=https://test-openapi.herokuapp.com/oauth&scope=public,birthday,email'));
 router.get('/oauth', (req, res) => {
     var code = req.query.code;
     var postBody = {
