5319189825c1f08f046126faba1e162d47f3b68b
Tuan Nguyen <nmtuan.dev@gmail.com>
2017-04-13 13:25:10 +0700

    bump

---
+++ configs.js
@@ -0,0 +1,23 @@
+module.exports = {
+    // "client_id": "uTLNa9VyuHLwp1Mw",
+    // "client_secret": "UDPNOmVYHUAdTwyV5RPlc5tcob7xRi2N",
+    // "redirect_uri": "http://localhost:3000/callback",
+    // "dialog_url": "http://localhost:2200/auth/dialog/authorize",
+
+    "client_id": "7kEp62Au6ZE8rO8a",
+    "client_secret": "UBjoUb3CYSWrsMeL0NbarDu28GmGHjzF",
+    "redirect_uri": "http://localhost:3000/callback",
+    "dialog_url": "https://api.misfitwearables.com/auth/dialog/authorize",
+
+    "thirdparty": {
+        "client_id": "dDWF2C5R5KZNImK3",
+        "client_secret": "nyuiGjveNJElCBOGkkRcsIOy5WMurpmv",
+        "redirect_uri": "http://localhost:2300/open/thirdparty/auth/exchange",
+        "dialog_url": "http://localhost:2300/open/thirdparty/auth/dialog/access",
+
+        // "client_id": "7kEp62Au6ZE8rO8a",
+        // "client_secret": "UBjoUb3CYSWrsMeL0NbarDu28GmGHjzF",
+        // "redirect_uri": "http://api.misfitwearables.com/open/thirdparty/auth/exchange",
+        // "dialog_url": "http://api.misfitwearables.com/open/thirdparty/auth/dialog/access",
+    }
+};
