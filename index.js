ba254ff3ccb499efc475d2d48a23f232f4767c48
Tuan Nguyen <nmtuan.dev@gmail.com>
2017-04-13 13:49:08 +0700

    bump

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ var app = express();
 app.use(morgan('dev'));
 // app.use(bodyParser.raw());
 // app.use(bodyParser.text());
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 app.use((err, req, res, next) => {
